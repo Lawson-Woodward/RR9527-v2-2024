dcfc7f92007c8c1da192e945637e2f6e3c408189
Lawson-Woodward <lawsonwoodward53@gmail.com>
2024-01-25 18:00:45 -0600

    roadrunnerrrrr

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/DriveConstants.java
@@ -56,7 +56,7 @@ public class DriveConstants {
      */
     public static double kV = 0.016;
     public static double kA = 0.003;
-    public static double kStatic = 0.01;
+    public static double kStatic = 0.01; // "yayyyyyyy" - first sign of life in roadrunner 1/25/2024 at exactly 6:00 P.M.
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
